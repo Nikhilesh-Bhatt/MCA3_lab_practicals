5a7f7c0b880aaeb177c3d0643580cfb61ad3a60b
Nikhilesh-Bhatt <85735215+Nikhilesh-Bhatt@users.noreply.github.com>
2021-12-05 21:45:03 +0530

    Create q2SlicingIndexing.py

---
+++ MCA3A/NIKHILESH_BHATT_20711046/LabWork4_Numpy/q2SlicingIndexing.py
@@ -0,0 +1,12 @@
+import numpy as np
+#INDEXING 
+a = np.arange(10, 1, -2)
+print("\n A sequential array with a negative step: \n",a)
+
+b = np.arange(20)
+print("\n Array is:\n ",b)
+ 
+#SLICING
+print("\n b[-8:17:1] = ",b[-8:17:1])
+
+print("\n b[10:] = ",b[10:])
