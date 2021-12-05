7fa2b1532819f5094597b877128dde0b54281614
Nikhilesh-Bhatt <85735215+Nikhilesh-Bhatt@users.noreply.github.com>
2021-12-05 21:41:48 +0530

    Create q1Arithmetic.py

---
+++ MCA3A/NIKHILESH_BHATT_20711046/LabWork4_Numpy/q1Arithmetic.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+arr = np.array([[5, 10], [15, 20]])
+# Add 10 to element values
+print("Adding 10: " + repr(arr + 10))
+
+# Multiple elements by 5
+print("Multiplying by 5: " + repr(arr * 5))
+
+# Subtract 5 from elements
+print("Subtracting 5: " + repr(arr - 5))
+
+# Matrix multiplication
+arr1 = np.array([[-8, 7], [17, 20], [8, -16], [11, 4]])
+arr2 = np.array([[5, -5, 10, 20], [-8, 0, 13, 2]])
+print("Multiplying matrix: " + repr(np.matmul(arr1, arr2)))
+
+# Exponential
+arr3 = np.array([[1, 5], [2.5, 2]])
+# Exponential of each element
+print("exponential: " + repr(np.exp(arr3)))
+
